fe359e2aee7191656cc351f35b7f10d8b937be2a
JCapito <joshuaecapito22@gmail.com>
2024-09-20 11:37:21 +0100

    fix: Wrapped Sports in the motion.dev element so that the whole Link element is included in the animation

--- components/Sport.tsx
+++ components/Sport.tsx
@@ -18,42 +18,38 @@ const Sport = () => {
         const inView = useInView(ref, { once: true });
 
         return (
-          <Link
-            href={url}
-            key={title}
-            className={`flex bg-neutral-950 p-7 rounded-lg items-center mb-10 ${
-              isEven ? "lg:flex-row-reverse" : ""
-            }`}
+          <motion.div
+            ref={ref}
+            initial={{ opacity: 0, x: isEven ? 200 : -200 }}
+            animate={inView ? { opacity: 1, x: 0 } : {}}
+            transition={{ duration: 0.6, ease: "easeOut" }}
+            className="mb-10"
           >
-            <motion.div
-              ref={ref}
-              initial={{ opacity: 0, x: isEven ? 200 : -200 }} // Start outside of viewport
-              animate={inView ? { opacity: 1, x: 0 } : {}} // Slide in from the side
-              transition={{ duration: 0.6, ease: "easeOut" }}
-              className="flex-1 px-4"
+            <Link
+              href={url}
+              key={title}
+              className={`flex bg-neutral-950 p-7 rounded-lg items-center ${
+                isEven ? "lg:flex-row-reverse" : ""
+              }`}
             >
-              <h1 className="text-3xl lg:text-8xl font-bold mb-2 text-red-500">
-                {title}
-              </h1>
-              <p className="text-gray-600 mb-4">{description}</p>
-              <Button label="Learn more" url={url} />
-            </motion.div>
+              <div className="flex-1 px-4">
+                <h1 className="text-3xl lg:text-8xl font-bold mb-2 text-red-500">
+                  {title}
+                </h1>
+                <p className="text-gray-600 mb-4">{description}</p>
+                <Button label="Learn more" url={url} />
+              </div>
 
-            <motion.div
-              ref={ref}
-              initial={{ opacity: 0, x: isEven ? -200 : 200 }} // Opposite direction for image
-              animate={inView ? { opacity: 1, x: 0 } : {}} // Slide in from the side
-              transition={{ duration: 0.6, ease: "easeOut" }}
-              className="relative w-80 h-96 flex-1"
-            >
-              <Image
-                className="object-cover rounded-lg"
-                src={image}
-                fill
-                alt={title}
-              />
-            </motion.div>
-          </Link>
+              <div className="relative w-80 h-96 flex-1">
+                <Image
+                  className="object-cover rounded-lg"
+                  src={image}
+                  fill
+                  alt={title}
+                />
+              </div>
+            </Link>
+          </motion.div>
         );
       })}
     </div>
