982b21216b996158934b4a884f38bbc63a157b7f
JCapito <joshuaecapito22@gmail.com>
2024-09-20 09:45:20 +0100

    feature: Implemented feature for the Sports Section to animate in based on viewport using framer-motion

--- components/Sport.tsx
+++ components/Sport.tsx
@@ -5,6 +5,8 @@ import { sportsList } from "@/constants/links";
 import Link from "next/link";
 import Image from "next/image";
 import Button from "./Button";
+import { motion, useInView } from "framer-motion";
+import { useRef } from "react";
 
 const Sport = () => {
   return (
@@ -12,6 +14,9 @@ const Sport = () => {
       {sportsList.map((item, index) => {
         const { title, description, image, url } = item;
         const isEven = index % 2 === 1;
+        const ref = useRef(null);
+        const inView = useInView(ref, { once: true });
+
         return (
           <Link
             href={url}
@@ -20,21 +25,34 @@ const Sport = () => {
               isEven ? "lg:flex-row-reverse" : ""
             }`}
           >
-            <div className="flex-1 px-4">
+            <motion.div
+              ref={ref}
+              initial={{ opacity: 0, x: isEven ? 200 : -200 }} // Start outside of viewport
+              animate={inView ? { opacity: 1, x: 0 } : {}} // Slide in from the side
+              transition={{ duration: 0.6, ease: "easeOut" }}
+              className="flex-1 px-4"
+            >
               <h1 className="text-3xl lg:text-8xl font-bold mb-2 text-red-500">
                 {title}
               </h1>
               <p className="text-gray-600 mb-4">{description}</p>
               <Button label="Learn more" url={url} />
-            </div>
-            <div className="relative w-80 h-96 flex-1">
+            </motion.div>
+
+            <motion.div
+              ref={ref}
+              initial={{ opacity: 0, x: isEven ? -200 : 200 }} // Opposite direction for image
+              animate={inView ? { opacity: 1, x: 0 } : {}} // Slide in from the side
+              transition={{ duration: 0.6, ease: "easeOut" }}
+              className="relative w-80 h-96 flex-1"
+            >
               <Image
                 className="object-cover rounded-lg"
                 src={image}
                 fill
                 alt={title}
               />
-            </div>
+            </motion.div>
           </Link>
         );
       })}
