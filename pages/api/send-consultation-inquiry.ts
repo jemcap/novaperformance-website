5aaf7ed85ffa36078c8592d7442241d497a61c79
JCapito <joshuaecapito22@gmail.com>
2024-10-01 19:43:39 +0100

    fix: Wrapped transporter in a Promise to handle error handling more efficiently

--- pages/api/send-consultation-inquiry.ts
+++ pages/api/send-consultation-inquiry.ts
@@ -1,12 +1,12 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import nodemailer from "nodemailer";
+import nodemailer, { Transporter } from "nodemailer"; 
 import Cors from "cors";
 
-// Initialize CORS middleware
+
 const cors = Cors({
-  methods: ['POST', 'GET', 'HEAD'], // Allowed methods
-  origin: process.env.NEXT_PUBLIC_API_URL || '*', // Allow specific origins or use '*' for all
-  credentials: true, // Allow credentials if needed
+  methods: ['POST', 'GET', 'HEAD'], 
+  origin: process.env.NEXT_PUBLIC_API_URL || '*', 
+  credentials: true, 
 });
 
 // Helper method to run CORS middleware
@@ -19,9 +19,23 @@ const runCors = (req: NextApiRequest, res: NextApiResponse) =>
   });
 
 type ResponseData = {
-    message: string;
-    error?: string;
-}
+  message: string;
+  error?: string;
+};
+
+// Wrap sendMail in a Promise
+const sendMail = (transporter: Transporter, mailData: nodemailer.SendMailOptions) => {
+  return new Promise((resolve, reject) => {
+    transporter.sendMail(mailData, (err, info) => {
+      if (err) {
+        console.error(err);
+        reject(err);
+      } else {
+        resolve(info);
+      }
+    });
+  });
+};
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse<ResponseData>) {
   // Run CORS middleware
@@ -43,14 +57,16 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
       },
     });
 
+    const mailData: nodemailer.SendMailOptions = {
+      from: email,
+      to: process.env.RECIPIENT_EMAIL,
+      subject: `Potential Client — New Consultation Inquiry from ${name}`,
+      text: `Hello,\n\nI'm interested in joining Nova Performance and would love to discuss membership options that best suit my needs.\n\nPlease feel free to reach out at your earliest convenience via:\n\nEmail: ${email}\n\nMobile: ${mobile}\n\nI look forward to hearing from you. Thank you!\n\nBest regards,\n\n${name}`,
+    };
+
     try {
-      // Send the email
-      await transporter.sendMail({
-        from: email,
-        to: process.env.RECIPIENT_EMAIL,
-        subject: `Potential Client — New Consultation Inquiry from ${name}`,
-        text: `Hello,\n\nI'm interested in joining Nova Performance and would love to discuss membership options that best suit my needs.\n\nPlease feel free to reach out at your earliest convenience via:\n\nEmail: ${email}\n\nMobile: ${mobile}\n\nI look forward to hearing from you. Thank you!\n\nBest regards,\n\n${name}`,
-      });
+      // Send the email using the wrapped Promise
+      await sendMail(transporter, mailData);
 
       res.status(200).json({ message: "Email sent successfully!" });
     } catch (error) {
