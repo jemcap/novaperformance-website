3e737d32b6c38bd2d4826bb95279bbcd23c369fc
JCapito <joshuaecapito22@gmail.com>
2024-10-02 11:22:57 +0100

    fix: Contact — Made the text larger + responsive

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -7,18 +7,18 @@ const ContactPage = () => {
     <div className="min-h-screen w-full flex items-center justify-center overflow-hidden bg-neutral-900 px-6 py-12 lg:px-10 lg:py-16">
       <div className="container max-w-6xl flex flex-col justify-center items-center gap-10 md:gap-12">
         <div>
-          <h1 className="text-4xl md:text-5xl lg:text-8xl mb-6 md:mb-8 text-center lg:text-left">
+          <h1 className="text-6xl md:text-7xl lg:text-8xl mb-6 md:mb-8 text-center lg:text-left">
             Contact Us
           </h1>
         </div>
         <div className="flex flex-col lg:flex-row gap-8 items-center justify-center text-center lg:text-left">
           <ContactForm />
           <aside className="text-lg md:text-xl lg:text-3xl flex flex-col gap-4 lg:gap-6 items-center lg:items-start">
-            <h1>...Or</h1>
-            <h1 className="text-red-500 text-3xl md:text-4xl lg:text-7xl">
+            <h1 className="text-4xl md:text-5xl lg:text-6xl">...Or</h1>
+            <h1 className="text-red-500 text-6xl md:text-7xl lg:text-8xl">
               VISIT US
             </h1>
-            <p className="max-w-xs md:max-w-sm lg:max-w-md leading-relaxed">
+            <p className="max-w-xs md:max-w-sm lg:max-w-md leading-relaxed text-3xl md:text-4xl lg:text-5xl">
               <a
                 href="https://maps.app.goo.gl/NQBXJnvYF2wQ6YDv5"
                 target="_blank"
@@ -29,7 +29,7 @@ const ContactPage = () => {
                 PE27 3NJ
               </a>
             </p>
-            <p className="text-lg md:text-xl">+447845 718581</p>
+            <p className="text-3xl md:text-4xl lg:text-5xl">+447845 718581</p>
           </aside>
         </div>
         <div className="flex flex-col justify-center items-center py-4 md:py-5">
